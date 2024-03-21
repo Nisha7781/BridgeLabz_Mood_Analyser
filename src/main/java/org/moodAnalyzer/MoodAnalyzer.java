f8693290dd350d2bfa9d223b15554a05ba4141c3
Nisha7781 <nishamali0781@gmail.com>
2024-03-21 13:29:59 +0530

    Refactored code to take mood message in constructor

--- src/main/java/org/moodAnalyzer/MoodAnalyzer.java
+++ src/main/java/org/moodAnalyzer/MoodAnalyzer.java
@@ -2,6 +2,15 @@ package org.moodAnalyzer;
 
 public class MoodAnalyzer
 {
+    public String s;
+
+    MoodAnalyzer()
+    {
+    }
+    MoodAnalyzer(String s)
+    {
+        this.s = s;
+    }
     public String moodAnalysis(String s)
     {
         if (s.equalsIgnoreCase("I am in sad mood"))
